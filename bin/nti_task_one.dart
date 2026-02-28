e317cf0c6c604676f83bea3b8f1e5c454218ce77
mohamedgomaa20 <20monagy@gmail.com>
2026-03-01 00:17:09 +0200

    Add comments to main function explaining branch usage for problem solving

--- bin/nti_task_one.dart
+++ bin/nti_task_one.dart
@@ -1,5 +1,10 @@
 import 'package:nti_task_one/nti_task_one.dart' as nti_task_one;
 
 void main(List<String> arguments) {
-  print('Hello world');
+ /*
+ Each problem has been solved in a separate branch.
+ Each problem has been solved in a separate branch.
+ Each problem has been solved in a separate branch.
+ Each problem has been solved in a separate branch.
+  */
 }
