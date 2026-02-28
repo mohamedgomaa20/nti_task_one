1aa2eea23f20c0977808278263224ab51e3a7268
mohamedgomaa20 <20monagy@gmail.com>
2026-03-01 01:40:51 +0200

    Question_2: Refactor main function to accept user input for product price, premium status, and coupon availability

--- bin/nti_task_one.dart
+++ bin/nti_task_one.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:nti_task_one/nti_task_one.dart' as nti_task_one;
 
 /*
@@ -5,16 +7,29 @@ import 'package:nti_task_one/nti_task_one.dart' as nti_task_one;
 gets 15% discount if premium member OR has coupon. Return final price.
  */
 void main(List<String> arguments) {
-  print(calculateFinalPrice(100, true, false));
-  print(calculateFinalPrice(100, false, true));
-  print(calculateFinalPrice(100, true, true));
-  print(calculateFinalPrice(100, false, false));
+  print("Enter Product Price:");
+  double productPrice = double.parse(stdin.readLineSync()!);
+  if (productPrice < 0) {
+    print("Invalid Product Price");
+    return;
+  }
+  print("Is your account premium? (y/n):");
+  bool isPremium = stdin.readLineSync()?.toLowerCase() == 'y';
+  print("Do you have a coupon? (y/n):");
+  bool hasCoupon = stdin.readLineSync()?.toLowerCase() == 'y';
+
+  double finalPrice = calculateFinalPrice(
+    productPrice: productPrice,
+    isPremium: isPremium,
+    hasCoupon: hasCoupon,
+  );
+  print("Final Price is: $finalPrice");
 }
 
-double calculateFinalPrice(
-  double productPrice,
-  bool isPremium,
-  bool hasCoupon,
-) {
+double calculateFinalPrice({
+  required double productPrice,
+  required bool isPremium,
+  required bool hasCoupon,
+}) {
   return (isPremium || hasCoupon) ? productPrice * 0.85 : productPrice;
 }
