4f6ef652ce72fa8d6b50190c76b8a36093057d16
mohamedgomaa20 <20monagy@gmail.com>
2026-02-28 21:39:12 +0200

    Implement login function with username, password, and account status validation

--- bin/nti_task_one.dart
+++ bin/nti_task_one.dart
@@ -1,5 +1,21 @@
 import 'package:nti_task_one/nti_task_one.dart' as nti_task_one;
 
+/*
+1 Create a function that receives username, password, and isAccountActive. Access is
+granted only if username equals 'student', password equals 'iti123', and account is
+active. Return 'Login Successful' or 'Access Denied'.
+ */
 void main(List<String> arguments) {
-  print('Hello world');
+  print(login("Mohamed", "1245", true));
+  print(login("Gomaa", "1245", false));
+  print(login("student", "iti123", false));
+  print(login("student", "iti123", true));
+}
+
+String login(String username, String password, bool isAccountActive) {
+  if (username == "student" && password == "iti123" && isAccountActive) {
+    return "Login Successful";
+  } else {
+    return "Access Denied";
+  }
 }
