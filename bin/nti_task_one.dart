67de97e88a22fae32c108cf15f418f7eb63ce292
mohamedgomaa20 <20monagy@gmail.com>
2026-02-28 23:29:00 +0200

    Question_8: Implement checkElectricityUsage function to categorize electricity consumption based on usage in kWh

--- bin/nti_task_one.dart
+++ bin/nti_task_one.dart
@@ -1,16 +1,25 @@
 import 'package:nti_task_one/nti_task_one.dart' as nti_task_one;
 
 /*
-7 Create a function that receives hasAccessCard and knowsPassword. Door opens only
-if both are true. Return 'Door Opened' or 'Access Restricted'.
+8 Create a function that receives electricity usage (kWh). usage < 200 → 'Low
+Consumption', 200–500 → 'Medium Consumption', above 500 → 'High Consumption'.
+Return the category.
  */
 void main(List<String> arguments) {
-  print(checkDoorAccess(true, true));
-  print(checkDoorAccess(true, false));
-  print(checkDoorAccess(false, true));
-  print(checkDoorAccess(false, false));
+  print(checkElectricityUsage(100));
+  print(checkElectricityUsage(300));
+  print(checkElectricityUsage(600));
+  print(checkElectricityUsage(-1000));
 }
 
-String checkDoorAccess(bool hasAccessCard, bool knowsPassword) {
-  return (hasAccessCard && knowsPassword) ? "Door Opened" : "Access Restricted";
+String checkElectricityUsage(double electricityUsageInKWh) {
+  if (electricityUsageInKWh < 0) {
+    return "Invalid Usage";
+  } else if (electricityUsageInKWh < 200) {
+    return "Low Consumption";
+  } else if (electricityUsageInKWh <= 500) {
+    return "Medium Consumption";
+  } else {
+    return "High Consumption";
+  }
 }
