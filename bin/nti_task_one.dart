3bfb69def21e3f126be4f0b086a5cf034d8d4b82
mohamedgomaa20 <20monagy@gmail.com>
2026-03-01 01:21:36 +0200

    Question_1: Enhance login functionality with user input for username, password, and account status

--- bin/nti_task_one.dart
+++ bin/nti_task_one.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:nti_task_one/nti_task_one.dart' as nti_task_one;
 
 /*
@@ -6,14 +8,24 @@ granted only if username equals 'student', password equals 'iti123', and account
 active. Return 'Login Successful' or 'Access Denied'.
  */
 void main(List<String> arguments) {
-  print(login("Mohamed", "1245", true));
-  print(login("Gomaa", "1245", false));
-  print(login("student", "iti123", false));
-  print(login("student", "iti123", true));
+  print("Enter Your username:");
+  String username = stdin.readLineSync()!;
+
+  print("Enter Your password:");
+  String password = stdin.readLineSync()!;
+
+  print("Enter Your account status:(true or false)");
+  bool isAccountActive = stdin.readLineSync()?.toLowerCase() == "true";
+
+  print("==================");
+  print(login(username, password, isAccountActive));
+  print("==================");
 }
 
 String login(String username, String password, bool isAccountActive) {
-  if (username == "student" && password == "iti123" && isAccountActive) {
+  if (username.toLowerCase() == "student" &&
+      password.toLowerCase() == "iti123" &&
+      isAccountActive) {
     return "Login Successful";
   } else {
     return "Access Denied";
