3d1693a7db1452491ef54dd66ebfd411f9a25922
mohamedgomaa20 <20monagy@gmail.com>
2026-02-28 21:28:21 +0200

    Initial commit

---
+++ bin/nti_task_one.dart
@@ -0,0 +1,5 @@
+import 'package:nti_task_one/nti_task_one.dart' as nti_task_one;
+
+void main(List<String> arguments) {
+  print('Hello world');
+}
---
+++ test/nti_task_one_test.dart
@@ -0,0 +1,8 @@
+import 'package:nti_task_one/nti_task_one.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
