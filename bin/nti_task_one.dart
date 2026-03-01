c427c8337ec0c20dd8f3a4ee6f145c828bf1141e
mohamedgomaa20 <20monagy@gmail.com>
2026-03-01 03:08:37 +0200

    Question_8: Enhance checkElectricityUsage function to prompt user input and display category based on electricity usage

--- bin/nti_task_one.dart
+++ bin/nti_task_one.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:nti_task_one/nti_task_one.dart' as nti_task_one;
 
 /*
@@ -6,16 +8,25 @@ Consumption', 200–500 → 'Medium Consumption', above 500 → 'High Consumptio
 Return the category.
  */
 void main(List<String> arguments) {
-  print(checkElectricityUsage(100));
-  print(checkElectricityUsage(300));
-  print(checkElectricityUsage(600));
-  print(checkElectricityUsage(-1000));
+  print("Enter Your electricity usage (kWh):");
+  double electricityUsageInKWh = double.parse(stdin.readLineSync()!);
+
+  if (electricityUsageInKWh < 0) {
+    print("--------------------------");
+    print("Invalid electricity usage.");
+    print("--------------------------");
+    return;
+  }
+
+  String category = checkElectricityUsage(electricityUsageInKWh);
+
+  print("------------------------------------------");
+  print("Electricity Usage Category: $category");
+  print("------------------------------------------");
 }
 
 String checkElectricityUsage(double electricityUsageInKWh) {
-  if (electricityUsageInKWh < 0) {
-    return "Invalid Usage";
-  } else if (electricityUsageInKWh < 200) {
+  if (electricityUsageInKWh < 200) {
     return "Low Consumption";
   } else if (electricityUsageInKWh <= 500) {
     return "Medium Consumption";
